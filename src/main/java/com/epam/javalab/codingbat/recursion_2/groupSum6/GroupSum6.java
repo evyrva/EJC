aaac75067e23f0f01677e50628c50453edfc4acd
Egor <32036210+evyrva@users.noreply.github.com>
2017-11-23 16:58:50 +0300

    Added part of codingbat recursion-2 tasks

---
+++ src/main/java/com/epam/javalab/codingbat/recursion_2/groupSum6/GroupSum6.java
@@ -0,0 +1,23 @@
+package com.epam.javalab.codingbat.recursion_2.groupSum6;
+
+public class GroupSum6 {
+    /**
+     * Given an array of ints, is it possible to choose a group of some of the ints, beginning at the start index,
+     * such that the group sums to the given target? However, with the additional constraint that all 6's must
+     * be chosen. (No loops needed.)
+
+     groupSum6(0, [5, 6, 2], 8) → true
+     groupSum6(0, [5, 6, 2], 9) → false
+     groupSum6(0, [5, 6, 2], 7) → false
+     */
+
+    public static void main(String[] args) {
+        int[] nums = {2, 4, 8};
+        System.out.println(groupSum6(0, nums, 13));
+    }
+
+    public static boolean groupSum6(int start, int[] nums, int target) {
+
+        return false;
+    }
+}
